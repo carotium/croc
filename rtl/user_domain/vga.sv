ca80060fef67faf8a1c4db0d3de4f9111f53c35d
carotium <coolboy9987@gmail.com>
2025-10-02 14:12:47 +0000

    hw: Added a simple VGA driver, which outputs color data from its input and generates sync signals for a 800x600@72Hz standard VGA display.

--- rtl/user_domain/vga.sv
+++ rtl/user_domain/vga.sv
@@ -1,8 +1,5 @@
 module vga(
-    // Genesys2 clk is 200 MHz
-    // Need to divide to 50 Mhz for 800 x 600 @ 72Hz
-    // with 50 MHz pixel clock
-    input wire clk_i_200M,
+    input wire clk_i_50M,
     input wire rstn_i,
     input wire [15:0] pcolor_i, //RGB565 format
     output wire hsync_o,
