dbe86cb44411d31820a9ea70c44ef3224a870f89
carotium <coolboy9987@gmail.com>
2025-10-02 14:11:38 +0000

    hw: Added a simple clock divider to be used by a VGA module, which requires a 50 MHz clock instead of the on chip 200 MHz clock provided by genesys2 board.

--- rtl/user_domain/clock_divider.sv
+++ rtl/user_domain/clock_divider.sv
@@ -1,22 +1,22 @@
 module clock_divider #(
-    parameter int unsigned DIVIDE_BY = 4
+    parameter int unsigned DIVIDE_BY = 2
 ) (
     input logic clk_200M_i,
     input logic rstn_i,
     output logic clk_50M_o
 );
     // 4 bit counter
-    logic [3:0] count = 0;
+    logic[1:0] count = 0;
 
-    always_ff @(posedge clk_200M_i or posedge rstn_i) begin
-        if(rstn_i) begin
-            clk_50M_o <= 1'b0;
+    assign clk_50M_o = count[1];
+
+    always_ff @(posedge clk_200M_i or negedge rstn_i) begin
+        if(!rstn_i) begin
             count = 0;
-        end else if(count == DIVIDE_BY - 1) begin
-            clk_50M_o <= ~clk_50M_o;
-            count <= 0;
-        end else begin
+        end else if(count < 3) begin
             count <= count + 1;
+        end else begin
+            count <= 0;
         end
     end
 endmodule
